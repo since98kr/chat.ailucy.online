3fb9df1e52face6d1e5983afb6abbbec39b2ccfa
since98kr <since98kr@gmail.com>
2026-07-18 13:05:15 +0900

    load giant step 2 UI refinements

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './chat-core.css';
+import './giant-step-2.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
